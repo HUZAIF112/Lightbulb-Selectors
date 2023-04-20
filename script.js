955e73868b09e6b484fa5e3d657b3efc3eab1748
Huzaif <huzaifsaiyed620@gmail.com>
2023-04-20 15:35:41 +0100

    done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Write your code here
+const lb1 = document.querySelector('#lightbulb1')
+const lb2 = document.querySelector('#lightbulb2')
+const lb3 = document.querySelector('#lightbulb3')
+const subtitle = document.querySelector('.subtitle')
+let count = 0
+
+lb1.addEventListener('click', function(){
+    count +=1
+    subtitle.innerHTML = `You've clicked the lights ${count} times.`
+    this.classList.toggle('active')
+})
+lb2.addEventListener('click', function(){
+    count +=1
+    subtitle.innerHTML = `You've clicked the lights ${count} times.`
+    this.classList.toggle('active')
+
+})
+lb3.addEventListener('click', function(){
+    count +=1
+    subtitle.innerHTML = `You've clicked the lights ${count} times.`
+    this.classList.toggle('active')
+})
+
